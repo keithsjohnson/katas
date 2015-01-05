ccb253a89e6f2b9ca5301d36ef4621a9537d8d5d
keithsjohnson <keithsjohnson@yahoo.com>
2015-01-05 20:39:34 +0000

    Potter Kata without Edge cases (2 sets of 4 books is cheaper than 1 set of 5 books and a set of 3)

--- katas/src/main/java/uk/co/keithsjohnson/katas/potter/PotterPriceCalculatorImpl.java
+++ katas/src/main/java/uk/co/keithsjohnson/katas/potter/PotterPriceCalculatorImpl.java
@@ -3,14 +3,14 @@ package uk.co.keithsjohnson.katas.potter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class PotterPriceCalculatorImpl {
 
 	public double price(int... books) {
-		Integer[] bookIntegers = toBooksInteger(books);
 
-		Map<Integer, List<Integer>> booksByBookMap = Arrays.stream(bookIntegers, 0, bookIntegers.length).collect(Collectors.groupingBy(book -> book));
+		Map<Integer, List<Integer>> booksByBookMap = Arrays.stream(books).boxed().collect(Collectors.groupingBy(book -> book));
 
 		if (booksByBookMap.size() == 0) {
 			return 0D;
@@ -20,18 +20,45 @@ public class PotterPriceCalculatorImpl {
 		}
 		System.out.println(booksByBookMap.toString());
 
-		double price = books.length * 8D;
+		double price = 0;
+		int bookCount = 0;
+		do {
+			bookCount = countAndRemoveOneBookFromEachList(booksByBookMap);
+			switch (bookCount) {
+			case 1:
+				price += bookCount * 8D;
+				break;
+			case 2:
+				price += bookCount * 8D * 0.95;
+				break;
+			case 3:
+				price += bookCount * 8D * 0.90;
+				break;
+			case 4:
+				price += bookCount * 8D * 0.80;
+				break;
+			case 5:
+				price += bookCount * 8D * 0.75;
+				break;
+			}
+		} while (bookCount > 0);
 
 		return price;
 	}
 
-	public static Integer[] toBooksInteger(int[] intBooks) {
+	private int countAndRemoveOneBookFromEachList(Map<Integer, List<Integer>> booksByBookMap) {
+		final AtomicInteger count = new AtomicInteger();
 
-		Integer[] result = new Integer[intBooks.length];
-		for (int i = 0; i < intBooks.length; i++) {
-			result[i] = Integer.valueOf(intBooks[i]);
-		}
-		return result;
+		booksByBookMap.keySet().forEach(key -> {
+			if (booksByBookMap.get(key).size() > 0) {
+				booksByBookMap.get(key).remove(0);
+				count.incrementAndGet();
+			}
+		});
+		System.out.println(booksByBookMap.toString());
+		System.out.println("Count=" + count.get());
+
+		return count.get();
 
 	}
 }
--- katas/src/test/java/uk/co/keithsjohnson/katas/potter/PotterPriceCalculatorImplJUnitTest.java
+++ katas/src/test/java/uk/co/keithsjohnson/katas/potter/PotterPriceCalculatorImplJUnitTest.java
@@ -3,6 +3,7 @@ package uk.co.keithsjohnson.katas.potter;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PotterPriceCalculatorImplJUnitTest {
@@ -62,25 +63,114 @@ public class PotterPriceCalculatorImplJUnitTest {
 		assertEquals(books.length * 8D, price, 0D);
 	}
 
-	// def testBasics
-	// assert_equal(8 * 2, price([0, 0]))
-	// assert_equal(8 * 3, price([1, 1, 1]))
-	// end
+	@Test
+	public void shouldCalculatePriceFor2DifferentBooks() {
+		// Given
+		int[] books = { 0, 1 };
 
-	// def testSimpleDiscounts
-	// assert_equal(8 * 2 * 0.95, price([0, 1]))
-	// assert_equal(8 * 3 * 0.9, price([0, 2, 4]))
-	// assert_equal(8 * 4 * 0.8, price([0, 1, 2, 4]))
-	// assert_equal(8 * 5 * 0.75, price([0, 1, 2, 3, 4]))
-	// end
+		// When
+		double price = potterPriceCalculatorImpl.price(books);
 
-	// def testSeveralDiscounts
-	// assert_equal(8 + (8 * 2 * 0.95), price([0, 0, 1]))
-	// assert_equal(2 * (8 * 2 * 0.95), price([0, 0, 1, 1]))
-	// assert_equal((8 * 4 * 0.8) + (8 * 2 * 0.95), price([0, 0, 1, 2, 2, 3]))
-	// assert_equal(8 + (8 * 5 * 0.75), price([0, 1, 1, 2, 3, 4]))
-	// end
+		// Then
+		assertEquals(8D * 2 * 0.95, price, 0D);
+	}
 
+	@Test
+	public void shouldCalculatePriceFor3DifferentBooks() {
+		// Given
+		int[] books = { 0, 2, 4 };
+
+		// When
+		double price = potterPriceCalculatorImpl.price(books);
+
+		// Then
+		assertEquals(8D * 3 * 0.9, price, 0D);
+	}
+
+	@Test
+	public void shouldCalculatePriceFor4DifferentBooks() {
+		// Given
+		int[] books = { 0, 1, 2, 4 };
+
+		// When
+		double price = potterPriceCalculatorImpl.price(books);
+
+		// Then
+		assertEquals(8D * 4 * 0.8, price, 0D);
+	}
+
+	@Test
+	public void shouldCalculatePriceFor5DifferentBooks() {
+		// Given
+		int[] books = { 0, 1, 2, 3, 4 };
+
+		// When
+		double price = potterPriceCalculatorImpl.price(books);
+
+		// Then
+		assertEquals(8D * 5 * 0.75, price, 0D);
+	}
+
+	@Test
+	public void shouldCalculatePriceFor3Books() {
+		// Given
+		int[] books = { 0, 0, 1 };
+
+		// When
+		double price = potterPriceCalculatorImpl.price(books);
+
+		// Then
+		assertEquals(8D + (8D * 2 * 0.95), price, 0D);
+	}
+
+	@Test
+	public void shouldCalculatePriceFor4Books() {
+		// Given
+		int[] books = { 0, 0, 1, 1 };
+
+		// When
+		double price = potterPriceCalculatorImpl.price(books);
+
+		// Then
+		assertEquals(2 * (8 * 2 * 0.95), price, 0D);
+	}
+
+	@Test
+	public void shouldCalculatePriceFor6Books() {
+		// Given
+		int[] books = { 0, 0, 1, 2, 2, 3 };
+
+		// When
+		double price = potterPriceCalculatorImpl.price(books);
+
+		// Then
+		assertEquals((8 * 4 * 0.8) + (8 * 2 * 0.95), price, 0D);
+	}
+
+	@Test
+	public void shouldCalculatePriceFor6MoreBooks() {
+		// Given
+		int[] books = { 0, 1, 1, 2, 3, 4 };
+
+		// When
+		double price = potterPriceCalculatorImpl.price(books);
+
+		// Then
+		assertEquals(8 + (8 * 5 * 0.75), price, 0D);
+	}
+
+	@Test
+	@Ignore
+	public void shouldCalculatePriceForEdgeCase1() {
+		// Given
+		int[] books = { 0, 0, 1, 1, 2, 2, 3, 4 };
+
+		// When
+		double price = potterPriceCalculatorImpl.price(books);
+
+		// Then
+		assertEquals(2 * (8 * 4 * 0.8), price, 0D);
+	}
 	// def testEdgeCases
 	// assert_equal(2 * (8 * 4 * 0.8), price([0, 0, 1, 1, 2, 2, 3, 4]))
 	// assert_equal(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),
